d8eb15ee16878a468d4258312290cce500d33c7d
gavrylenko denys <denys.gavrylenko@gmail.com>
2021-03-01 14:28:22 +0200

    feat: add all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import moment from "moment";
 
 function App() {
+  const renderImage = () => {
+    const imgArr = [];
+    for (let i = 1; i <= 12; i += 1) {
+      imgArr.push(
+        <img
+          className="images"
+          key={i}
+          src={`./img/${i}.webp`}
+          alt="pictures"
+        />
+      );
+    }
+    return imgArr;
+  };
+  const images = renderImage();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {/* <!-- Header --> */}
+
+      <header>
+        <div className="container">
+          <nav className="navigation">
+            {/* <!-- Logo is hidden till view port size 1200px  --> */}
+
+            {/* <h1 className="logo">logo</h1> */}
+
+            {/* <!-- Navigation --> */}
+          </nav>
+        </div>
       </header>
-    </div>
+
+      {/* <!-- main content --> */}
+
+      <main>
+        <section className="section">
+          <div className="container">
+            {/* <div class="backdrop is-hidden" data-modal>
+              <div class="modal full-picture">
+                <button class="button-close" data-modal-close>
+                  <svg class="cross-icon" width="11" height="11">
+                    <use href="./img/svg/sprite.svg#icon-cross"></use>
+                  </svg>
+                </button>
+              </div>
+            </div> */}
+            <div>
+              <span>
+                Количество картинок:
+                {images.length}.{" "}
+              </span>
+              <span>Сегодня </span> {moment().format("DD/MM/YYYY HH:mm")}
+            </div>
+            <div className="image-list">{images}</div>
+          </div>
+        </section>
+      </main>
+
+      {/* <!-- Footer (just because it should be even if it's epmty) --> */}
+      <footer>
+        <address></address>
+      </footer>
+    </>
   );
 }
 
